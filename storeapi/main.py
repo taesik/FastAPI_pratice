c35a25da9ecc353e072aa42ed1a2a45f6fc6b432
taesik mun <taesic12@knou.ac.kr>
2023-10-05 08:55:28 +0900

    post

--- storeapi/main.py
+++ storeapi/main.py
@@ -15,9 +15,15 @@ class UserPost(UserPostIn):
 post_table = {}
 
 
-@app.post('/', response_model=UserPost)
+@app.post('/post', response_model=UserPost)
 async def create_post(post: UserPostIn):
 	data = post.model_dump()
 	last_record_id = len(post_table)
 	new_post = {**data, 'id': last_record_id}
 	post_table[last_record_id] = new_post
+	return new_post
+
+
+@app.get('/post', response_model=list[UserPost])
+async def get_all_posts():
+	return list(post_table.values())
