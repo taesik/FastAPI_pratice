5c1e3e2de56f521a613a9b27b4b6efcac4c230ff
taesik mun <taesic12@knou.ac.kr>
2023-10-05 14:41:28 +0900

    comment post http request

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,29 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from storeapi.routers.post import router as post_router
 
 app = FastAPI()
-
-
-class UserPostIn(BaseModel):
-	body: str
-
-
-class UserPost(UserPostIn):
-	id: int
-
-
-post_table = {}
-
-
-@app.post('/post', response_model=UserPost)
-async def create_post(post: UserPostIn):
-	data = post.model_dump()
-	last_record_id = len(post_table)
-	new_post = {**data, 'id': last_record_id}
-	post_table[last_record_id] = new_post
-	return new_post
-
-
-@app.get('/post', response_model=list[UserPost])
-async def get_all_posts():
-	return list(post_table.values())
+app.include_router(post_router)
